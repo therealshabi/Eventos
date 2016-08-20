8721d7791571ce60e47c1005c162521ad96e5368
gurleensethi <sarusethi@rocketmail.com>
2016-08-20 12:25:39 +0530

    Add new event activity/fragment

---
+++ app/src/main/java/app/com/thetechnocafe/eventos/AddEventActivity.java
@@ -0,0 +1,23 @@
+package app.com.thetechnocafe.eventos;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.add_event_fragment_container);
+
+        if(fragment == null) {
+            fragment = AddEventFragment.getInstance();
+            fragmentManager.beginTransaction().add(R.id.add_event_fragment_container, fragment).commit();
+        }
+    }
+}
---
+++ app/src/main/java/app/com/thetechnocafe/eventos/AddEventFragment.java
@@ -0,0 +1,29 @@
+package app.com.thetechnocafe.eventos;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gurleensethi on 20/08/16.
+ */
+public class AddEventFragment extends Fragment {
+
+    public static AddEventFragment getInstance() {
+        return new AddEventFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
