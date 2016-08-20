482b6940b37e5c5e2bfca0841a278b0761d8ea3a
gurleensethi <sarusethi@rocketmail.com>
2016-08-20 13:14:09 +0530

    Update AddEventFragment

--- app/src/main/java/app/com/thetechnocafe/eventos/AddEventFragment.java
+++ app/src/main/java/app/com/thetechnocafe/eventos/AddEventFragment.java
@@ -2,8 +2,12 @@ package app.com.thetechnocafe.eventos;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -12,6 +16,8 @@ import android.view.ViewGroup;
  */
 public class AddEventFragment extends Fragment {
 
+    private FloatingActionButton mAddNewEventActionButton;
+
     public static AddEventFragment getInstance() {
         return new AddEventFragment();
     }
@@ -19,11 +25,45 @@ public class AddEventFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_add_event, container, false);
+
+        mAddNewEventActionButton = (FloatingActionButton) view.findViewById(R.id.fragment_add_event_fab);
+
+        //Set up FAB
+        mAddNewEventActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Set up toolbar
+        //Set up the toolbar
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.fragment_add_event_toolbar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        if (activity.getSupportActionBar() != null) {
+            activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            activity.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_back);
+        }
+
+        return view;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                getActivity().finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
