303e055b58e6e36a68652e50b8cf0b1cff3ac7c9
gurleensethi <sarusethi@rocketmail.com>
2016-08-21 21:43:07 +0530

    Add guideline option in add new event and text views

--- app/src/main/java/app/com/thetechnocafe/eventos/AddEventFragment.java
+++ app/src/main/java/app/com/thetechnocafe/eventos/AddEventFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,6 +38,7 @@ public class AddEventFragment extends Fragment {
         if (activity.getSupportActionBar() != null) {
             activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             activity.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_back);
+            activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
 
         return view;
@@ -50,5 +53,11 @@ public class AddEventFragment extends Fragment {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.add_new_event_menu, menu);
     }
 }
