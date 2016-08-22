fa7ecdabc50fd6827624bb93f3d300093f4d47f8
gurleensethi <sarusethi@rocketmail.com>
2016-08-22 16:33:08 +0530

    Add Time picker and Date picker to AddEvent Fragment

--- app/src/main/java/app/com/thetechnocafe/eventos/AddEventFragment.java
+++ app/src/main/java/app/com/thetechnocafe/eventos/AddEventFragment.java
@@ -1,5 +1,7 @@
 package app.com.thetechnocafe.eventos;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -18,16 +20,30 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Created by gurleensethi on 20/08/16.
  */
 public class AddEventFragment extends Fragment {
     private static final String TAG = "AddEventFragment";
+    private static final String DATE_PICKER_TAG = "datepicker";
+    private static final String TIME_PICKER_TAG = "timepicker";
+    private static final int DATE_PICKER_CODE = 1;
+    private static final int TIME_PICKER_CODE = 2;
+
     private TextView mInfoText;
     private ImageButton mAddContactImageButton;
     private LinearLayout mContactsContainer;
     private ImageButton mAddForumLinkImageButton;
     private LinearLayout mLinkContainer;
+    private TextView mDateText;
+    private TextView mTimeText;
+    private Date mEventDate;
+    private Date mEventDateTime;
+
     public static AddEventFragment getInstance() {
         return new AddEventFragment();
     }
@@ -48,6 +64,16 @@ public class AddEventFragment extends Fragment {
         mContactsContainer = (LinearLayout) view.findViewById(R.id.fragment_add_event_contacts_container);
         mAddForumLinkImageButton = (ImageButton) view.findViewById(R.id.fragment_add_event_add_forum_link);
         mLinkContainer = (LinearLayout) view.findViewById(R.id.fragment_add_event_forum_link_container);
+        mDateText = (TextView) view.findViewById(R.id.fragment_add_event_date);
+        mTimeText = (TextView) view.findViewById(R.id.fragment_add_event_time);
+
+        //Set up date and text
+        mEventDate = new GregorianCalendar().getTime();
+        setDateText(mEventDate);
+
+        //Set up time text
+        mEventDateTime = new GregorianCalendar().getTime();
+        setTimeText(mEventDateTime);
 
         //Set up the toolbar
         Toolbar toolbar = (Toolbar) view.findViewById(R.id.fragment_add_event_toolbar);
@@ -82,6 +108,26 @@ public class AddEventFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 addForumLinkToContainer();
+            }
+        });
+
+        //Set up on click listener for date text view (fire up DialogDatePicker)
+        mDateText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogDatePicker dialogDatePicker = DialogDatePicker.getInstance(mEventDate);
+                dialogDatePicker.setTargetFragment(AddEventFragment.this, DATE_PICKER_CODE);
+                dialogDatePicker.show(getFragmentManager(), DATE_PICKER_TAG);
+            }
+        });
+
+        //Set up on click listener for time text view (fire up DialogTimePicker)
+        mTimeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogTimePicker dialogTimePicker = DialogTimePicker.getInstance(mEventDateTime);
+                dialogTimePicker.setTargetFragment(AddEventFragment.this, TIME_PICKER_CODE);
+                dialogTimePicker.show(getFragmentManager(), TIME_PICKER_TAG);
             }
         });
 
@@ -131,5 +177,33 @@ public class AddEventFragment extends Fragment {
             }
         });
         mLinkContainer.addView(view);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK) {
+            if(requestCode == DATE_PICKER_CODE && data != null) {
+                mEventDate = DialogDatePicker.getDate(data);
+                setDateText(mEventDate);
+            } else if (requestCode == TIME_PICKER_CODE && data != null) {
+                mEventDateTime = DialogTimePicker.getTime(data);
+                setTimeText(mEventDateTime);
+            }
+        }
+    }
+
+    //Function to set the date text
+    private void setDateText(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM yyyy");
+        String dateText = simpleDateFormat.format(date);
+        mDateText.setText(dateText);
+    }
+
+    //Function to set time text
+    private void setTimeText(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm");
+        String timeText = simpleDateFormat.format(date);
+        mTimeText.setText(timeText);
     }
 }
